6f6f7121bf225416e652803e67c075981a94d87a
Andrew Locke <alocke12992@gmail.com>
2018-06-05 09:11:21 -0600

    Fixes Sidebar

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -4,10 +4,12 @@ import PortMap from 'components/PortMap';
 import SideBar from 'components/SideBar';
 
 export const HomeView = (store) => (
-  <div>
-    <SideBar className="map__nav" ports={store.ports} />
-    <h4>Port Map</h4>
-    <PortMap className="home--portMap" updatePorts={store.updatePorts} />
+  <div className="mainWrapper">
+    <SideBar className="sideBar" ports={store.ports} />
+    <div className="mapWrapper">
+      <h4>Port Map</h4>
+      <PortMap className="home--portMap" updatePorts={store.updatePorts} />
+    </div>
   </div>
 );
 
